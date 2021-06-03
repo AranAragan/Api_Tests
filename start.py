1f4133a38f3294b9e801777d3f0f579373aca0a8
AranAragan <aranaragan@gmail.com>
2021-06-03 12:44:34 +0300

    regress

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ subprocess.Popen(
     "python3 -m pytest -s tests/negative/test.py --alluredir results/negative", shell=True).wait()
 subprocess.Popen("allure serve results/negative",
                  shell=True)
-# subprocess.Popen(
-#     "python3 -m pytest -s tests/regress/test.py --alluredir results/regress", shell=True).wait()
-# subprocess.Popen("allure serve results/regress",
-#                  shell=True)
+subprocess.Popen(
+    "python3 -m pytest -s tests/regress/test.py --alluredir results/regress", shell=True).wait()
+subprocess.Popen("allure serve results/regress",
+                 shell=True)
