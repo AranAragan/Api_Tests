5207ec7d719adc1ed14c7b8dcc09e3ea12bf7a55
AranAragan <aranaragan@gmail.com>
2021-06-09 12:54:04 +0300

    Moved to docker

--- userdata/api_test/start.py
+++ userdata/api_test/start.py
@@ -6,10 +6,10 @@ import subprocess
 os.chdir(pathlib.Path(__file__).parent)
 shutil.rmtree("results", True)
 subprocess.Popen(
-    "python3 -m pytest -s tests/negative/test.py --alluredir results/negative", shell=True).wait()
+    "python -m pytest -s /api_test/tests/negative/test.py --alluredir /api_test/results/negative", shell=True).wait()
 subprocess.Popen("allure serve results/negative",
                  shell=True)
 subprocess.Popen(
-    "python3 -m pytest -s tests/regress/test.py --alluredir results/regress", shell=True).wait()
+    "python -m pytest -s /api_test/tests/regress/test.py --alluredir /api_test/results/regress", shell=True).wait()
 subprocess.Popen("allure serve results/regress",
                  shell=True)
