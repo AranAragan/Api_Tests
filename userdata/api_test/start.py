71f5c90ca0703b9dfe2e3c0a6fdc543c01bed452
AranAragan <aranaragan@gmail.com>
2021-06-09 13:30:33 +0300

    Updated

--- userdata/api_test/start.py
+++ userdata/api_test/start.py
@@ -4,12 +4,12 @@ import pathlib
 import subprocess
 
 os.chdir(pathlib.Path(__file__).parent)
-shutil.rmtree("results", True)
+shutil.rmtree("result", True)
 subprocess.Popen(
-    "python -m pytest -s /api_test/tests/negative/test.py --alluredir /api_test/results/negative", shell=True).wait()
-subprocess.Popen("allure serve results/negative",
+    "python -m pytest -s /api_test/tests/negative/test.py --alluredir /result/negative", shell=True).wait()
+subprocess.Popen("allure serve /result/negative",
                  shell=True)
 subprocess.Popen(
-    "python -m pytest -s /api_test/tests/regress/test.py --alluredir /api_test/results/regress", shell=True).wait()
-subprocess.Popen("allure serve results/regress",
+    "python -m pytest -s /api_test/tests/regress/test.py --alluredir /result/regress", shell=True).wait()
+subprocess.Popen("allure serve /result/regress",
                  shell=True)
